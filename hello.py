e3a97af2865e83913eea25f7d7a80e5dc37dab0a
Anumit Jooloor <anumit@gmail.com>
2024-06-29 00:46:19 -0700

    Add files

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 # /usr/local/bin/python3.12 hello.py
 # Hello
 # Python can be used to handle big data and perform complex mathematics.
+# git remote add origin https://github.com/anumit-web/Python-Basics-2024.git
 print("Hello, World!")
 
-    
+
 
 
